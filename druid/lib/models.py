b74ad5a4c0693da37436b6b4cfb256e6e61291c7
Khachatur Chichakyan <lj7538495162@gmail.com>
2024-02-18 17:10:15 +0300

    создание БД

--- druid/lib/models.py
+++ druid/lib/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Texts(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    link = models.CharField(max_length=50, verbose_name='Путь')
+
+    class Meta:
+        verbose_name_plural = 'Тексты'
+        verbose_name = 'Текст'
